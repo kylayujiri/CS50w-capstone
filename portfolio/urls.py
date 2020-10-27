6e5edeea2d33fb22d20f23d3a3a4e4fd17badcbe
Kyla Yujiri <27860923+kylayujiri@users.noreply.github.com>
2020-10-27 12:58:59 -0700

    Add profile and logout routes

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("logout", views.logout_view, name="logout"),
+    path("profile/<str:username>", views.profile, name="profile")
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,16 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    return render(request, "portfolio/layout.html")
+    return render(request, "portfolio/index.html")
+
+def profile(request, username):
+    # send profile header info here
+    return render(request, "portfolio/profile.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
