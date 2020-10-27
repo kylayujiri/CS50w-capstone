7fdd1c058df57885c9ae10d497bef820e3ac443d
Kyla Yujiri <27860923+kylayujiri@users.noreply.github.com>
2020-10-26 21:01:29 -0700

    Add Category to admin.py

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Artist, Work, Comment, Album
+from .models import Artist, Work, Comment, Album, Category
 
 # Register your models here.
 admin.site.register(Artist)
 admin.site.register(Work)
 admin.site.register(Comment)
 admin.site.register(Album)
+admin.site.register(Category)
